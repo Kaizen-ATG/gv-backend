41c09c2822464c2056cd79ee2ae2933aae0f1b9e
sreelathachunduru <lathasreechunduru@gmail.com>
2021-01-23 17:03:01 +0000

    Added and deployed GetUsers.

--- Handler.cs
+++ Handler.cs
@@ -1,31 +1,72 @@
 using Amazon.Lambda.Core;
+using System.Collections;
+using System.Collections.Generic;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Text.Json;
+using MySql.Data.MySqlClient;
+using System;
 
-[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-       public Response Hello(Request request)
-       {
-           return new Response("Hello GreenVibe!!", request);
-       }
+        private string DB_HOST = System.Environment.GetEnvironmentVariable("DB_HOST");
+        private string DB_NAME = System.Environment.GetEnvironmentVariable("DB_NAME");
+        private string DB_USER = System.Environment.GetEnvironmentVariable("DB_USER");
+        private string DB_PASSWORD = System.Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+        public APIGatewayProxyResponse GetUsers(APIGatewayProxyRequest request)
+        {
+            int userId = Int32.Parse(request.PathParameters["userId"]);
+            LambdaLogger.Log("Getting detials for: " + userId);
+
+            MySqlConnection connection = new MySqlConnection($"server={DB_HOST};user id={DB_USER};password={DB_PASSWORD};port=3306;database={DB_NAME};");
+            connection.Open();
+
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = @"SELECT * FROM `user` WHERE `user_id` = @userId";
+            cmd.Parameters.AddWithValue("@userId", userId);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+            ArrayList users = new ArrayList();
+
+            while (reader.Read())
+            {
+                User user = new User(reader.GetInt16("user_id"), reader.GetString("username"), reader.GetString("email"),reader.GetInt16("role_id"));
+                users.Add(user);
+            }
+
+            connection.Close();
+
+            return new APIGatewayProxyResponse
+            {
+                Body = JsonSerializer.Serialize(users),
+                Headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/json" },
+                { "Access-Control-Allow-Origin", "*" }
+            },
+                StatusCode = 200,
+            };
+        }
     }
 
-    public class Response
+    public class User
     {
-      public string Message {get; set;}
-      public Request Request {get; set;}
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int  Role { get; set; }
 
-      public Response(string message, Request request){
-        Message = message;
-        Request = request;
-      }
-    }
+        public User() { }
 
-    public class Request
-    {
-      public string Key1 {get; set;}
-      public string Key2 {get; set;}
-      public string Key3 {get; set;}
+        public User(int user_id, string username, string email,int role_id)
+        {
+            UserId = user_id;
+            UserName = username;
+            Email = email;
+            Role=role_id;
+        }
     }
 }

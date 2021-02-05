477f42231854230fba3d0b57ead0d404459480c7
sreelathachunduru <lathasreechunduru@gmail.com>
2021-02-05 16:25:56 +0000

    Issue Fix

--- Handler.cs
+++ Handler.cs
@@ -83,7 +83,7 @@ namespace AwsDotnetCsharp
             {
                 connection.Open();
                 var cmd = connection.CreateCommand();
-                cmd.CommandText = @"select * from redeemoffers order by points_required;";
+                cmd.CommandText = @"select * from redeemoffers where isactive=true order by points_required;";
                 MySqlDataReader reader = cmd.ExecuteReader();
                 ArrayList offers = new ArrayList();
                 while (reader.Read())
